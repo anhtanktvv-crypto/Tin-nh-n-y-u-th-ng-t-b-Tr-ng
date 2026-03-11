a9aa73433bb73b68d45ce73ccbd0590a7eae8ef5
TanHat <Tanhat@gmail.com>
2026-03-11 19:23:02 +0700

    Fix loi vao app va base href

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"327ed814500baca0480a02f8555aa84a9afe32
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3208321520" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1644405513" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
